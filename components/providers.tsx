923479b12c9e88f9a5ff6425eb3d203a3550778a
Enzo <enzotrr@gmail.com>
2025-01-01 12:10:21 -0300

    feat: change link to splitLink with process.env logic

--- components/providers.tsx
+++ components/providers.tsx
@@ -3,7 +3,7 @@
 import { PropsWithChildren, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { trpc } from "@/app/_trpc/client";
-import { httpBatchLink } from "@trpc/client";
+import { httpBatchLink, splitLink } from "@trpc/client";
 import { Toaster } from "./ui/sonner";
 import { ThemeProvider } from "next-themes";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
@@ -13,11 +13,18 @@ export function Providers({ children }: PropsWithChildren) {
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [
-        httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
-        }),
-        httpBatchLink({
-          url: "http://wallet-sim.vercel.app/api/trpc",
+        splitLink({
+          condition() {
+            return Boolean(
+              process.env.NEXT_PUBLIC_ENVIRONMENT === "development"
+            );
+          },
+          true: httpBatchLink({
+            url: "http://localhost:3000/api/trpc",
+          }),
+          false: httpBatchLink({
+            url: "http://wallet-sim.vercel.app/api/trpc",
+          }),
         }),
       ],
     })
